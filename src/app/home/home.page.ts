b75ce75f422419d93931f8ef0ba068925f6d4e9b
Manikandan A <116253047+00Maddy@users.noreply.github.com>
2023-04-03 16:21:30 +0530

    3/4/2023

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,9 +1,10 @@
-import { Component, ElementRef, ViewChild,OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
 import { jqxComboBoxComponent } from 'jqwidgets-ng/jqxcombobox';
-import { HttpClient } from '@angular/common/http';  
+import { HttpClient } from '@angular/common/http';
 import { IonContent } from '@ionic/angular';
 import { Observable } from 'rxjs';
 import { LoadingController } from '@ionic/angular';
+
 
 
 @Component({
@@ -11,94 +12,108 @@ import { LoadingController } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit{
+export class HomePage implements OnInit {
   myData: any;
-  viewCard:boolean=false;
+  viewCard: boolean = false;
   selectedItem: any;
-  values:any=[];
   showSwipeModal: boolean = false;
-  
-  user:any;
+  entryTime: any;
+  user: any;
+  Search: any = "Search for IMEI's...";
   selectedValue = 'today';
   @ViewChild('myComboBox') myComboBox!: jqxComboBoxComponent;
   @ViewChild('selectionlog') selectionlog!: ElementRef;
   @ViewChild(IonContent) content!: IonContent;
-  rawdata!: any;
-  totalDate!: any;
   rawDataList: any;
   listOfRawData: any;
-  constructor(private http: HttpClient,private loadingController: LoadingController ){}
+  eventTimestamp: any;
+  constructor(private http: HttpClient, private loadingController: LoadingController) { }
   ngOnInit() {
     this.http.get('http://localhost:8080/loggingevent/imei').subscribe(data => {
       this.myData = data;
       console.log(this.myData);
     });
-    
+
   }
   trackModal(selectedData: any) {
     let date = new Date();
-    
+
     let toTime = new Date(date.getTime() - (date.getTimezoneOffset() * 60000)).toJSON();
     this.selectedValue = selectedData;
     let fromTime = '00:00:00'
-     if (selectedData === 'today') {
-        this.user = {
-          "fromDate": (new Date(date.getTime() - (date.getTimezoneOffset() * 60000)).toJSON()).split('T')[0] + 'T' + fromTime.substring(0, 9),
-          "toDate": toTime.split("T")[0] + 'T' + date.toTimeString().split(' ')[0],
-        }
-        console.log(this.user.fromDate,this.user.toDate);
-      } else if(selectedData=== 'yesterday') {
-        let yesterday = new Date(date.setDate(date.getDate() - 1));
-        this.user = {
-          "fromDate": (new Date(yesterday.getTime() - (yesterday.getTimezoneOffset() * 60000)).toJSON()).split('T')[0] + 'T' + fromTime,
-          "toDate": ( new Date(yesterday.getTime() - (yesterday.getTimezoneOffset() * 60000)).toJSON()).split('T')[0] + 'T23:59:59'
-        }
-      } 
+    if (selectedData === 'today') {
+      this.user = {
+        "fromDate": (new Date(date.getTime() - (date.getTimezoneOffset() * 60000)).toJSON()).split('T')[0] + 'T' + fromTime.substring(0, 9),
+        "toDate": toTime.split("T")[0] + 'T' + date.toTimeString().split(' ')[0],
+      }
+      console.log(this.user.fromDate, this.user.toDate);
+    } else if (selectedData === 'yesterday') {
+      let yesterday = new Date(date.setDate(date.getDate() - 1));
+      this.user = {
+        "fromDate": (new Date(yesterday.getTime() - (yesterday.getTimezoneOffset() * 60000)).toJSON()).split('T')[0] + 'T' + fromTime,
+        "toDate": (new Date(yesterday.getTime() - (yesterday.getTimezoneOffset() * 60000)).toJSON()).split('T')[0] + 'T23:59:59'
+      }
+    }
   }
-    myComboBoxOnSelect(event: any): void {
-        if (event.args) {
-          this.selectedItem = event.args.item;
-          console.log(this.selectedItem.value);
-          this.values=this.selectedItem.value;
-          this.showSwipeModal=true;
-        }
-    };
+  myComboBoxOnSelect(event: any): void {
+    if (event.args) {
+      this.selectedItem = event.args.item;
+      console.log(this.selectedItem.value);
+      this.showSwipeModal = true;
+    }
+  };
 
-    async OnShow() {
-      let imei = this.selectedItem.value;
-      let startDate = this.user.fromDate;
-      let endDate = this.user.toDate;
-      console.log(imei,startDate,endDate);
-      await this.presentLoading();  
-      let url = `http://localhost:8080/loggingevent/rawdata`;
-      
-      const data$: Observable<any> = this.http.get<any>(`${url}/${imei}/${startDate}/${endDate}`);
-      data$.subscribe({
-        next: (response: any) => {
-          this.rawDataList = response;
-          this.listOfRawData = this.rawDataList.map((item: any) => JSON.parse(item).rawData);
-          this.loadingController.dismiss();
-          console.log(this.listOfRawData);
-        },
-        error: (err: any) => {
-          console.error(err);
-          this.loadingController.dismiss();
-        },
-        complete: () => {
-          console.log('Completed');
-          this.viewCard=true;
-        }
-      });
-    }
-    scrollTop() {
-      this.content.scrollToTop(500);
-    }
+  async OnShow() {
+    let imei = this.selectedItem.value;
+    let startDate = this.user.fromDate;
+    let endDate = this.user.toDate;
+    console.log(imei, startDate, endDate);
+    await this.presentLoading();
+    let url = `http://localhost:8080/loggingevent/rawdata`;
 
-    async presentLoading() {
-      const loading = await this.loadingController.create({
-        message: 'Please wait...',
-      });
-      await loading.present();
-    }
-    
+    const data$: Observable<any> = this.http.get<any>(`${url}/${imei}/${startDate}/${endDate}`);
+    data$.subscribe({
+      next: (response: any) => {
+        this.rawDataList = response.map((item: any) => {
+          const parsedItem = JSON.parse(item[0]);
+          return parsedItem.rawData;
+        });
+        this.eventTimestamp = response.map((item: any) => {
+          return item[1];
+        });
+        console.log(this.rawDataList);
+
+
+        this.entryTime = this.eventTimestamp.map((timestamp: { split: (arg0: string) => [any, any]; }) => {
+          const [date, time] = timestamp.split('T');
+          const formattedTime = time.slice(0, -10);
+          return `${date} ${formattedTime}`;
+        });
+
+        console.log(this.entryTime);
+
+
+      },
+      error: (err: any) => {
+        console.log(err);
+        this.loadingController.dismiss();
+      },
+      complete: () => {
+        this.loadingController.dismiss();
+        console.log('Completed');
+        this.viewCard = true;
+      }
+    });
+
+  }
+  scrollTop() {
+    this.content.scrollToTop(500);
+  }
+
+  async presentLoading() {
+    const loading = await this.loadingController.create({
+      message: 'Please wait...',
+    });
+    await loading.present();
+  }
 }
